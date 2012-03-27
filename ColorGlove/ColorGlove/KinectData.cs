dd7b4ff440ae3edd7d924d74b3df846a88f118c0
Arun Ganesan <arunganesan123@gmail.com>
2012-03-27 18:22:05 -0400

    Moved hand gestures to util. Created serializable data container. Fixed small bug in cropping.

---
+++ ColorGlove/ColorGlove/KinectData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ColorGlove
+{
+    [Serializable()]
+    public class KinectData : ISerializable
+    {
+        private short[] _depth;
+        private byte[] _color;
+
+        int _width, _height;
+        int _stride = 4;
+
+        public byte[] color() { return _color; }
+        public short[] depth() { return _depth; }
+
+        public KinectData(int width = 640, int height = 480)
+        {
+            this._width = width;
+            this._height = height;
+
+            _depth = new short[width * height];
+            _color = new byte[width * height * _stride];
+        }
+
+        // Serialization interface
+        public KinectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            _depth = (short[])info.GetValue("depth", typeof(short[]));
+            _color = (byte[])info.GetValue("color", typeof(byte[]));
+        }
+
+        // Serialization interface
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("depth", _depth);
+            info.AddValue("color", _color);
+        }
+    }
+}
---
+++ ColorGlove/FeatureExtractionLib/Util.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace FeatureExtractionLib
+{
+    public class Util
+    {
+
+        public enum HandGestureFormat
+        {
+            Background = 0,
+            OpenHand = 1,
+            CloseHand = 2,
+            One = 3,
+            Two = 4,
+        };
+        
+        // Returns the XY position of the index.
+        public static Point toXY(int idx, int width, int height, int stride)
+        {
+            Point p = new Point();
+            p.X = (int)(idx / stride) % width;
+            p.Y = (int)(idx / stride) / width;
+            return p;
+        }
+
+        // Returns the linear index corresponding to the XY position
+        public static int toID(int x, int y, int width, int height, int stride)
+        {
+            return (y * width + x) * stride;
+        }
+    }
+}
