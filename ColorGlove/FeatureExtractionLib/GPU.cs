6a6a1faf6a989391e62b1965d4a1f4cd39f08036
Michael Zhang <chossing@gmail.com>
2012-04-07 21:23:07 -0400

    Going to see the cache hit if doing vector adding

--- ColorGlove/FeatureExtractionLib/GPU.cs
+++ ColorGlove/FeatureExtractionLib/GPU.cs
@@ -440,7 +440,7 @@ kernel void AddVectorWithTrees(
         };
         
         // Constructor function
-        public GPUCompute(ComputeModeFormat SetComputeMode = ComputeModeFormat.kTest)         
+        public GPUCompute(ComputeModeFormat SetComputeMode = ComputeModeFormat.kTest2D)         
         {
             ComputePlatform platform = ComputePlatform.Platforms[0];
             ComputeContextPropertyList properties = new ComputeContextPropertyList(platform);
