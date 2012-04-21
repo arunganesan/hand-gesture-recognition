5b2ffd9eada2643bb3d101137f734b5c64f4e630
Michael Zhang <chossing@gmail.com>
2012-04-20 21:15:32 -0400

    Add again

---
+++ ColorGlove/ColorGlove/SetContent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+namespace ColorGlove
+{
+    class SetContent
+    {
+        public static MainWindow main_window_;
+        public static void SetMainWindowControler(MainWindow main_window){
+            main_window_ = main_window; 
+        }
+        public static void SetMetaInformation(string s)
+        {
+           
+            main_window_.Dispatcher.BeginInvoke(new Action(() =>
+            //main_window_.MetaLabel.Dispatcher.BeginInvoke(new Action(() =>
+           {
+              main_window_.MetaLabel.Content = s;            }));
+              
+        }
+        public static void SetColorImageLabel(string s)
+        {
+
+            main_window_.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                main_window_.colorImageLabel.Text = s;
+            }));
+        }
+        public static void SetDepthImageLabel(string s)
+        {
+            main_window_.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                main_window_.depthImageLabel.Text = s;
+            })); 
+        }
+        public static void SetPoolImageLabel(string s)
+        {
+            main_window_.Dispatcher.BeginInvoke(new Action(() =>
+           {
+               main_window_.poolImageLabel.Text = s;
+           })); 
+       }
+ 
+    }
+}
