b16bccdb68497c0dd2327fedd67fee46dbb8e56e
Arun Ganesan <arunganesan123@gmail.com>
2012-04-22 00:07:35 -0400

    Connected wheel to websocket client.

--- HandsSVG/ws4py_mouse.py
+++ HandsSVG/ws4py_mouse.py
@@ -5,9 +5,13 @@ from ws4py.client.threadedclient import WebSocketClient
 def move(x, y): win32api.SetCursorPos((x, y))
 def up(x, y): win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
 def down(x, y): win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+def wheel(d): win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL, 0, 0, d, 0)
 
 class MouseClient(WebSocketClient):
     def opened(self):
+        self.scaleX = 10;
+        self.scaleY = 5;
+        self.state = "OpenHand"
         print "Opened"
     
     def closed(self, code, reason):
@@ -15,23 +19,29 @@ class MouseClient(WebSocketClient):
     
     def received_message(self, m):
         print "=> %d %s" % (len(m), str(m))
-        m = re.match(r'\(.*,(.*),(.*),(.*)\)', str(m))
+        m = re.match(r'\((.*),(.*),(.*),(.*)\)', str(m))
 
-        scaleX = 10;
-        scaleY = 5;
+        gesture = m.group(1)
+        x = int(m.group(2))
+        y = int(m.group(3))
+        depth = int(m.group(4))
+        
+        scaledX = x*self.scaleX;
+        scaledY = y*self.scaleY;
+        
+        if (self.state != gesture):
+            self.state = gesture;
+            if (gesture == "OpenHand"): up(scaledX, scaledY)
+            else: down(scaledX, scaledY)
 
-        x = int(m.group(1))
-        y = int(m.group(2))
-        depth = int(m.group(3))
+        move(x*self.scaleX, y*self.scaleY)
         
-        move(x*scaleX, y*scaleY)
-        
-        if len(str(m)) == 175:
-            self.close(reason='Bye bye')
+        if (depth < 800): wheel(20)
+        elif (depth > 1500): wheel(-20)
 
 if __name__ == '__main__':
     try:
-        ws = MouseClient('ws://lessentropy.net:9876/', protocols=['sample'])
+        ws = MouseClient('ws://localhost:8181/', protocols=['sample'])
         ws.daemon = False
         ws.connect()
     except KeyboardInterrupt:
